08df065f5079cd8c68f96cfd2f60c7d171946f18
Serthow <oryxlag@gmail.com>
2020-07-10 16:28:23 -0600

    Update OnDeath.java

--- src/main/java/me/kc/thirdplugin/events/OnDeath.java
+++ src/main/java/me/kc/thirdplugin/events/OnDeath.java
@@ -1,14 +1,46 @@
 package me.kc.thirdplugin.events;
 
+import org.bukkit.EntityEffect;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.PistonMoveReaction;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Pose;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRecipeDiscoverEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class OnDeath implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e){
+
         e.getEntity().sendMessage("You Suck!");
     }
 
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e){
+
+    }
 }
