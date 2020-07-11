a822e22cace83e8482964ec1885329b662d1444f
Serthow <oryxlag@gmail.com>
2020-07-11 13:59:46 -0600

    Murder Completed

--- src/main/java/me/kc/thirdplugin/commands/Murder.java
+++ src/main/java/me/kc/thirdplugin/commands/Murder.java
@@ -1,5 +1,7 @@
 package me.kc.thirdplugin.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,14 +13,22 @@ public class Murder implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if(!(sender instanceof Player)){
+            System.out.println("Run This Command As A Player");
             return false;
         }
+        Player player = (Player) sender;
         if(args.length == 0){
-            //
+            player.sendMessage("You Killed Yourself");
+            player.setHealth(0);
         }
+        Player target = Bukkit.getPlayerExact(args[0]);
+        if (target == null){
+            target.sendMessage(ChatColor.DARK_RED + "Player Could Not be Found");
+            return false;
+        }
+        target.sendMessage(player.getDisplayName() + " Murdered you");
+        target.setHealth(0);
 
-
-
-        return false;
+        return true;
     }
 }
